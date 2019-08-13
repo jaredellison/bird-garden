2b799b3401f778740fa6022f2f9d36200446f70f
Jared Ellison <jaredellison@gmail.com>
2019-08-13 16:00:23 -0400

    Update control labels

--- client/bird-ui.js
+++ client/bird-ui.js
@@ -3,54 +3,52 @@
 
 document.body.innerHTML += `
 <div id="control-container">
+  <h3>Volume</h3>
   <input type="range" id="volume" min="0" max="1" step=".01" value="0">
-  <label for="volume">volume</label>
 
-  <h2>Controls</h2>
-  <h3>F</h3>
+  <h3>Frequency</h3>
   <input type="range" id="ifrq" name="ifrq" min="0" max="1" step=".01" value="0">
-  <label for="ifrq">ifrq</label>
 
-  <h3>amp</h3>
+  <h3>Amplitude</h3>
   <input type="range" id="amp-atk" min="0" max="1" step=".01" value="0">
-  <label for="amp-atk">amp-atk</label>
+  <label for="amp-atk">Attack</label>
   <input type="range" id="amp-dcy" min="0" max="1" step=".01" value="0">
-  <label for="amp-dcy">amp-dcy</label>
+  <label for="amp-dcy">Decay</label>
 
-  <h3>f-modulation</h3>
+  <h3>Frequency Modulation</h3>
   <div>
     <input type="range" id="fmod1" min="0" max="1" step=".01" value="0">
-    <label for="fmod1">fmod1</label>
+    <label for="fmod1">Modulation 1</label>
     <input type="range" id="atkf1" min="0" max="1" step=".01" value="0.01">
-    <label for="atkf1">atkf1</label>
+    <label for="atkf1">Attack</label>
     <input type="range" id="dcyf1" min="0" max="1" step=".01" value="0.01">
-    <label for="dcyf1">dcyf1</label>
+    <label for="dcyf1">Decay</label>
   </div>
   <div>
     <input type="range" id="fmod2" min="0" max="1" step=".01" value="0">
-    <label for="fmod2">fmod2</label>
+    <label for="fmod2">Modulation 2</label>
     <input type="range" id="atkf2" min="0" max="1" step=".01" value="0.01">
-    <label for="atkf2">atkf2</label>
+    <label for="atkf2">Attack</label>
     <input type="range" id="dcyf2" min="0" max="1" step=".01" value="0.01">
-    <label for="dcyf2">dcyf2</label>
+    <label for="dcyf2">Decay</label>
   </div>
 
-  <h3>a-modulation</h3>
+  <h3>Amplitude Modulation</h3>
   <div>
     <input type="range" id="amod1" min="0" max="1" step=".01" value="0">
-    <label for="amod1">amod1</label>
+    <label for="amod1">Modulation 1</label>
     <input type="range" id="atka1" min="0" max="1" step=".01" value="0.01">
-    <label for="atka1">atka1</label>
+    <label for="atka1">Attack</label>
     <input type="range" id="dcya1" min="0" max="1" step=".01" value="0.01">
-    <label for="dcya1">dcya1</label>
+    <label for="dcya1">Decay</label>
   </div>
   <div>
     <input type="range" id="amod2" min="0" max="1" step=".01" value="0">
-    <label for="amod2">amod2</label>
+    <label for="amod2">Modulation 2</label>
     <input type="range" id="atka2" min="0" max="1" step=".01" value="0.01">
-    <label for="atka2">atka2</label>
+    <label for="atka2">Attack</label>
     <input type="range" id="dcya2" min="0" max="1" step=".01" value="0.01">
-    <label for="dcya2">dcya2</label>
+    <label for="dcya2">Decay</label>
   </div>
 </div>
 
