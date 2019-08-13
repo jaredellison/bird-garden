a928b53a0c5ddd159d45235469a225d560ff3fff
Jared Ellison <jaredellison@gmail.com>
2019-08-13 14:14:14 -0400

    Use random interval between chirps and prevent initial click sound

--- client/bird-action.js
+++ client/bird-action.js
@@ -1,15 +1,19 @@
 const startChirping = () => {
   window.bird = new Bird();
+  // Chirp at random interval
   randomChirp();
-  setInterval(() => {
-    window.bird.trigger();
-  }, 1000);
+
+  // Chirp at regular interval
+  // setInterval(() => {
+  //   window.bird.trigger();
+  // }, 1000);
 
   document.querySelector('tone-oscilloscope').bind(bird.voice.output);
 };
 
 let started = false;
 
+// An initiall interaction is required to start audio
 let volumeSlider = document.querySelector('#volume');
 volumeSlider.addEventListener('input', e => {
   if (!started) {
@@ -17,12 +21,16 @@ volumeSlider.addEventListener('input', e => {
     Tone.context.resume();
     startChirping();
   }
+
   masterVolume.volume.value = dbScale(e.target.value);
+  setTimeout(() => {
+    masterVolume.mute = false;
+  }, 1000);
 });
 
 
 let randomChirp = () => {
-  let time = Math.random() * 2500 + 100;
+  let time = Math.random() * 5000 + 100;
   setTimeout(() => {
     window.bird.trigger();
     randomChirp();
