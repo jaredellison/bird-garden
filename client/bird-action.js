29da6fd9d1c4c49a646343996f738a97b29fe01d
Jared Ellison <jaredellison@gmail.com>
2019-08-14 13:54:38 -0400

    Bind oscilloscope on intial interaction

--- client/bird-action.js
+++ client/bird-action.js
@@ -26,16 +26,19 @@ document.querySelector('#volume').addEventListener('input', e => {
   if (!started) {
     window.bird = new Bird();
     started = true;
+
+    // Allow audio to start
     Tone.context.resume();
+
     // Unmute master volume and start chirping
     setTimeout(() => {
       masterVolume.mute = false;
       startChirping();
     }, 2000);
+
+    // Connect oscilloscope for visualization
+    document.querySelector('tone-oscilloscope').bind(bird.voice.output);
   }
 
   masterVolume.volume.value = dbScale(e.target.value);
-
-  // Connect oscilloscope
-  document.querySelector('tone-oscilloscope').bind(bird.voice.output);
 });
